853f21a3df14983482a3cc98c13a4f663f455f7e
Kuldyaev <slavalion1@gmail.com>
2022-04-21 09:59:00 +0300

    Store page almost finished

--- models/Product.php
+++ models/Product.php
@@ -4,28 +4,37 @@ namespace app\models;
 
 class Product extends DBModel
 {
-    protected $id;
-    protected $name;
-    protected $description;
-    protected $price;
+    public $id;
+    public $name;
+    public $s_description;
+    public $f_description;
+    public $price;
+    public $quantity;
+    public $q_limit;
 
     protected $props = [
         'name' => false,
-        'description' => false,
-        'price' => false
+        's_description' => false,
+        'f_description' => false,
+        'quantity' => false,
+        'price' => false,
+        'q_limit' => false
     ];
 
 
     public function __construct($name = null, $description = null, $price = null)
     {
         $this->name = $name;
-        $this->description = $description;
+        $this->s_description = $s_description;
+        $this->f_description = $f_description;
+        $this->quantity = $quantity;
         $this->price = $price;
+        $this->q_limit = $q_limit;
     }
 
 
     protected static function getTableName()
     {
-        return 'products';
+        return 'goods';
     }
 }
--- views/product/catalog.php
+++ views/product/catalog.php
@@ -1,11 +1,28 @@
-<h2>Каталог</h2>
-
-<?php foreach ($catalog as $item):?>
-    <div>
-        <h3><a href="/?c=product&a=card&id=<?=$item['id']?>"><?=$item['name']?></a></h3>
-        <p>price: <?=$item['price']?></p>
-        <button>Купить</button>
-    </div>
-<?php endforeach;?>
-
+<div class="pageName">
+    <h3>eStore</h3>
+</div>
+<div class="cardDesk">
+    <?php foreach ($catalog as $item):?>
+        <div class="itemCard">
+            <a href="/item/?id=<?=$item['id']?>" class="itemLink">
+                <img src="images/imgs_goods_300/<?=$item['id']?>.jpg" alt="small img" class="imgimg">
+            </a>
+            <div class="itemInfo">
+                <a href="/item/?id=<?=$item['id']?>" class="itemLink">
+                    <div class="itemName">
+                        <?=$item['name']?>
+                    </div>
+                    <div class="itemPrice">
+                        цена: <?=$item['price']?> руб.
+                    </div>
+                </a>
+                <div class="buyBtn">
+                    <button class="buy" data-itemid=<?=$item['id']?> data-session=<?=session_id()?>>BUY</button>
+                </div>        
+            </div>
+        </div>
+    <?php endforeach;?>
+</div>
+<div class="moreBtn center" style="width:100%">
 <a href="/?c=product&a=catalog&page=<?=$page?>">Еще</a>
+</div>
