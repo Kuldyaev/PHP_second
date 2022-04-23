47895b8d0ef71787c7088d53715791a389829f1e
Kuldyaev <slavalion1@gmail.com>
2022-04-23 19:51:21 +0300

    TWIG was installed

--- public/index.php
+++ public/index.php
@@ -1,14 +1,17 @@
 <?php
+session_start();
 //   подключаем файл конфигурации
 include $_SERVER['DOCUMENT_ROOT'] . "/../config/config.php";
 //   читаем адресную строку
 $url_arr = explode('/', $_SERVER['REQUEST_URI']);
 
-
+use app\engine\Autoload;
 use app\models\{Product, User};
 use app\engine\Render;
+use app\engine\TwigRender;
 
 include  ROOT . "/engine/Autoload.php";
+require_once ROOT . '/vendor/autoload.php';
 
 spl_autoload_register([new Autoload(), 'loadClass']);
 
@@ -18,7 +21,7 @@ $actionName = $_GET['a'];
 $controllerClass = CONTROLLER_NAMESPACE . ucfirst($controllerName) . "Controller";
 
 if (class_exists($controllerClass)) {
-    $controller = new $controllerClass(new Render());
+    $controller = new $controllerClass(new TwigRender());
     $controller->runAction($actionName);
 } else {
     die("Нет такого контроллера");
